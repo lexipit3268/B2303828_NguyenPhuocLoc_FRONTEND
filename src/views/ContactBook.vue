60bc1f0ee2452eeac34c9e39646497d1bc8acf9e
Nguyen Loc <locdedoda@gmail.com>
2026-01-22 14:43:41 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -27,6 +27,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh
+          </span>
+        </router-link>
       </div>
     </div>
   </div>
@@ -36,6 +46,7 @@ import ContactCard from "@/components/ContactCard.vue";
 import InputSearch from "@/components/InputSearch.vue";
 import ContactList from "@/components/ContactList.vue";
 import ContactService from "@/services/contact.service";
+import { param } from "jquery";
 export default {
   components: {
     ContactCard,
